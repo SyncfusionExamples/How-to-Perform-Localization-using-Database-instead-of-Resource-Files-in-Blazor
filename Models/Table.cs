d0054e87d951ecd875b87f22c00a90fcbec581f2
kmuthukumarmkm <muthukumar.kannan@syncfusion.com>
2022-01-13 12:25:11 +0530

    sample upload

---
+++ Models/Table.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BlazorServer.Models
+{
+    public partial class Table
+    {
+        public string Key { get; set; }
+        public string EnUs { get; set; }
+        public string De { get; set; }
+    }
+}
